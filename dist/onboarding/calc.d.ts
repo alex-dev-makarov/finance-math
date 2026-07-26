a17f164b65e864412d6e01974c5aff136f227581
alex-dev-makarov <sasha.makarov.28.1998@gmail.com>
2026-07-26 21:49:38 +0300

    chore: remove dist from git tracking

--- dist/onboarding/calc.d.ts
+++
@@ -1,6 +0,0 @@
-import type { ICategoryInput, IEmergencyCushionParams, IRuleOf150Params } from './types';
-export declare const calculateCategoriesTotal: (categories: readonly ICategoryInput[]) => number;
-export declare const calculateLeftoverIncome: (monthlyIncome: number, categoriesTotal: number) => number;
-export declare const calculateEmergencyCushion: ({ monthlyExpenses, cushionMonths, }: IEmergencyCushionParams) => number;
-export declare const calculateTargetCapital: ({ monthlyExpenses, }: IRuleOf150Params) => number;
-export declare const calculatePassiveIncome: (capitalAmount: number) => number;
